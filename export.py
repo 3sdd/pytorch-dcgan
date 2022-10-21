39742beb914545611db639103961877c8aa9c83b
3sdd <56486103+3sdd@users.noreply.github.com>
2022-10-21 16:15:22 +0900

    Create export.py

---
+++ export.py
@@ -0,0 +1,26 @@
+import argparse
+import torch
+from models import Generator
+
+
+def get_parser():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--generator-path',type=str,default='./results/g.pth')
+    parser.add_argument('--out-path',type=str,default='./results/dcgan-generator.onnx')
+    return parser
+
+def export():
+    args=get_parser().parse_args()
+
+    dummy_input = torch.randn(1,100,1,1)
+    model = Generator()
+    model.load_state_dict(torch.load(args.generator_path,map_location='cpu'))
+
+    input_names = [ "input" ]
+    output_names = [ "output" ]
+
+    torch.onnx.export(model, dummy_input, args.out_path, verbose=True, input_names=input_names, output_names=output_names)
+    print("finish")
+
+if __name__=="__main__":
+    export()
